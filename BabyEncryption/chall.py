8b25f54d2fcca9fd3d06bc90329bfc6e4912b1c4
Amirali Rafie <66920260+Null-byte-00@users.noreply.github.com>
2024-01-06 04:31:04 +0000

    Add files via upload

---
+++ BabyEncryption/chall.py
@@ -0,0 +1,16 @@
+import string
+from secret import MSG
+
+def encryption(msg):
+    ct = []
+    for char in msg:
+        ct.append((123 * char + 18) % 256)
+    return bytes(ct)
+
+ct = encryption(MSG)
+f = open('./msg.enc','w')
+f.write(ct.hex())
+f.close()
+
+
+# 123*x + 18 % 256 = n
